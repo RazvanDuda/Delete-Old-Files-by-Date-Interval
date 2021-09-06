cc605661ee1526b17bd96eb0a93f955ab5554a1a
RazvanDuda <razvan.duda@gmail.com>
2021-09-07 00:42:55 +0100

    Fixing my logic mistakes

--- delete.nim
+++ delete.nim
@@ -18,19 +18,18 @@ proc GetTodayDate(): string =
     let dt_new = parse(today_date, "yyyy-MM-dd")
     return dt_new.format("dd-MM-yyyy")
 
-proc GenerateDeleteDate(number_of_days_to_deduct:int): string =
-    let today = (now() - number_of_days_to_deduct.days).getDateStr
-    let dt = parse(today, "yyyy-MM-dd")
-    return dt.format("dd-MM-yyyy")
+proc NumberOfDaysFromCreation(file_date:string): int64 =
+        let date = now()
+        let deb = parse(file_date, "dd-MM-yyyy")
+        return (date - deb).inDays
 
-var DeleteDate = GenerateDeleteDate(number_of_days)
 var rollingLog = newRollingFileLogger(fmt"{logs_full_path}/{GetTodayDate()}.log")
 
 for f in walkFiles(fmt"{files_full_path}/*.{file_extension}"):
     addHandler(rollingLog)
-    var w_date_time = f.getCreationTime 
-    var w_date = w_date_time.format("dd-MM-yyyy")
-    echo w_date
-    if w_date <= DeleteDate:
+    var calc_number_of_days = NumberOfDaysFromCreation(f.getCreationTime.utc.format("dd-MM-yyyy")) 
+    if calc_number_of_days > number_of_days:
         removeFile(f)
         info(fmt"File: {f} , deleted on {GetTodayDate()} .")
+    else:
+        echo(fmt"File: {f} , is not matching criteria.")
